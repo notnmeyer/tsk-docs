4d28c00f9a3999d152f1855255d58997a5b985ff
Nate Meyer <672246+notnmeyer@users.noreply.github.com>
2024-03-31 20:48:53 -0700

    dont show homepage features
    dont show homepage features

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,9 +38,6 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
